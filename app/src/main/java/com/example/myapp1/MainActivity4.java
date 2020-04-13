19d29678bdddb721bde24072b035319d262504df
Fany <fatinker1703@hotmail.com>
2020-04-13 09:15:11 -0500

    Fourth Commit

--- app/src/main/java/com/example/myapp1/MainActivity4.java
+++ app/src/main/java/com/example/myapp1/MainActivity4.java
@@ -3,12 +3,112 @@ package com.example.myapp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    String mes = getIntent().getStringExtra("mes");
+    String dia = getIntent().getStringExtra("dia");
+    EditText etTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        int diaN = Integer.parseInt(dia);
+        int mesN = Integer.parseInt(mes);
+        String signo = getSigno(mesN,diaN);
+        etTexto.setText(signo);
+    }
+
+    public static String getSigno(int mes, int dia){
+        String signo = "";
+        switch (mes) {
+            case 1:
+                if (dia > 21) {
+                    signo = "ACUARIO (Ene 20 - Feb 18)";
+                } else {
+                    signo = "CAPRICORNIO (Dic 22 - Ene 19)";
+                }
+                break;
+            case 2:
+                if (dia > 20) {
+                    signo = "PISCIS (Feb 19 - Mar 20)";
+                } else {
+                    signo = "ACUARIO (Ene 20 - Feb 18)";
+                }
+                break;
+            case 3:
+                if (dia > 22) {
+                    signo = "ARIES (Mar 21 - Abr 19)";
+                } else {
+                    signo = "PISCIS (Feb 19 - Mar 20)";
+                }
+                break;
+            case 4:
+                if (dia > 21) {
+                    signo = "TAURO (Abr 20 - May 20)";
+                } else {
+                    signo = "ARIES (Mar 21 - Abr 19)";
+                }
+                break;
+            case 5:
+                if (dia > 22) {
+                    signo = "GEMINIS (May 21 - Jun 20)";
+                } else {
+                    signo = "TAURO (Abr 20 - May 20)";
+                }
+                break;
+            case 6:
+                if (dia > 22) {
+                    signo = "CANCER (Jun 21 - Jul 22)";
+                } else {
+                    signo = "GEMINIS (May 21 - Jun 20)";
+                }
+                break;
+            case 7:
+                if (dia > 24) {
+                    signo = "LEO (Jul 23 - Ago 22)";
+                } else {
+                    signo = "CANCER (Jun 21 - Jul 22)";
+                }
+                break;
+            case 8:
+                if (dia > 24) {
+                    signo = "VIRGO (Ago 23 - Sep 22)";
+                } else {
+                    signo = "LEO (Jul 23 - Ago 22)";
+                }
+                break;
+            case 9:
+                if (dia > 24) {
+                    signo = "LIBRA (Sep 23 - Oct 22)";
+                } else {
+                    signo = "VIRGO (Ago 23 - Sep 22)";
+                }
+                break;
+            case 10:
+                if (dia > 24) {
+                    signo = "ESCORPION (Oct 23 - Nov 21)";
+                } else {
+                    signo = "LIBRA (Sep 23 - Oct 22)";
+                }
+                break;
+            case 11:
+                if (dia > 23) {
+                    signo = "SAGITARIO (Nov 22 - Dic 21)";
+                } else {
+                    signo = "ESCORPION (Oct 23 - Nov 21)";
+                }
+                break;
+            case 12:
+                if (dia > 23) {
+                    signo = "CAPRICORNIO (Dic 22 - Ene 19)";
+                } else {
+                    signo = "SAGITARIO (Nov 22 - Dic 21)";
+                }
+                break;
+        }
+        return signo;
     }
 }
--- app/src/main/java/com/example/myapp1/MainActivity5.java
+++ app/src/main/java/com/example/myapp1/MainActivity5.java
@@ -3,12 +3,38 @@ package com.example.myapp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity5 extends AppCompatActivity {
+
+    String anio = getIntent().getStringExtra("año");
+    TextView tvChino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        int anioN = Integer.parseInt(anio);
+        final String horoscopo = getAnimal(anioN);
+        tvChino.setText(horoscopo);
+    }
+    public static String getAnimal(int anio){
+        String animal = "";
+        int resto = anio % 12;
+        switch (resto) {
+            case 0: animal = "Mono"; break;
+            case 1: animal = "Gallo"; break;
+            case 2: animal = "Perro"; break;
+            case 3: animal = "Cerdo"; break;
+            case 4: animal = "Rata"; break;
+            case 5: animal = "Buey"; break;
+            case 6: animal = "Tigre"; break;
+            case 7: animal = "Conejo"; break;
+            case 8: animal = "Dragon"; break;
+            case 9: animal = "Serpiente"; break;
+            case 10: animal = "Caballo"; break;
+            case 11: animal = "Cabra"; break;
+        }
+        return animal;
     }
 }
